517e076ed371db0f6acce9ccaa9033f289eec561
Elfaz <elfazab2100@gmail.com>
2025-03-04 18:42:09 +0300

    finalizing...

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,13 +7,7 @@ const Home = () => (
   <div className="home-container">
     <Navbar />
     <h2 className="welcome-title">Welcome to the Currency Exchange App</h2>
-    <p>
-      Here, you can find the exchange rates of all Ethiopian banks. This
-      resource enables you to compare rates among different banks, helping you
-      make informed decisions with ease. With access to up-to-date exchange
-      rates, you can manage your currency transactions effectively and choose
-      the best options that suit your financial needs.
-    </p>
+
     <p className="description">
       Check the latest exchange rates to make smarter choices for travel,
       business, or personal use.
